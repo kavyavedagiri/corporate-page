5dfefd327dd3ef4efac5381eca826e60b7df9ea1
kavyavedagiri <kavyav1997@gmail.com>
2021-04-23 17:39:41 +0530

    alignment

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   dividerFullWidth: {
-    margin: `0px`,
+    margin: `5px 10px 15px 5px`,
     backgroundColor:" rgb(240 9 81)",
     
   },
